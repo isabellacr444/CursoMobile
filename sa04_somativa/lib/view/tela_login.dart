3f53dde6a84d009d839f79422f1c5e05294fbf7a
senaisp5@gmail.com <senaisp5@gmail.com>
2025-04-29 13:18:11 -0300

    29.04.2025

--- sa04_somativa/lib/view/tela_login.dart
+++ sa04_somativa/lib/view/tela_login.dart
@@ -1,25 +1,29 @@
 import 'package:flutter/material.dart';
 
-class TelaLogin extends StatelessWidget { // Utilizado quando a tela não é dinâmica
+class TelaLogin extends StatelessWidget { // É um widget "StatelessWidget" utilizado quando a tela não precisa ser atualizada com dados dinâmicamente
   @override
+  // Dentro do "build" colocamos um Scaffold para estruturar a nossa tela
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple, // Definimos a cor de fundo
       body: Padding(
         padding: const EdgeInsets.all(32.0), // Definimos um padding para o espaço interno
-        child: Column( // Organizar os elementos em coluna
-          mainAxisAlignment: MainAxisAlignment.center, // centraliza os elementos
+        child: Column( // Organizar os widget filhos em uma coluna vertical
+          mainAxisAlignment: MainAxisAlignment.center, // centraliza os elementos na tela
           children: [ 
+            // Exibe o texto "APP" com o estilo de font: 32 e cor branca
             Text('App', style: TextStyle(fontSize: 32, color: Colors.white)),
-            SizedBox(height: 40),
+            SizedBox(height: 40), // Espaçamento entre o titulo e os campos de texto
+            // "TextField" para a entrada de dados do email
             TextField(
               decoration: InputDecoration(
                 labelText: 'Email',
-                filled: true, // Para esconder os caracteries da senha
-                fillColor: Colors.white,
+                filled: true, // Garante que o campo tenha colorido
+                fillColor: Colors.white,// Cor de fundo 
               ),
             ),
-            SizedBox(height: 16),
+            SizedBox(height: 16),// Espaçamento entre a entrada de dados do email
+            // Exibe campo para usuario digitar a senha
             TextField(
               obscureText: true,
               decoration: InputDecoration(
@@ -28,15 +32,17 @@ class TelaLogin extends StatelessWidget { // Utilizado quando a tela não é din
                 fillColor: Colors.white,
               ),
             ),
-            SizedBox(height: 32),
+            SizedBox(height: 32),// Espaçamento entra os campos de email e senha e o botão de login
+            // Executa a ação definida em onPressed
             ElevatedButton(
               onPressed: () {
-                Navigator.pushNamed(context, "/tela02");
+                Navigator.pushNamed(context, "/tela02"); // Define que ao ser clicado o botão leva a tela02
               },
-              child: Text('LOGIN'),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white,
-                foregroundColor: Colors.blue,
+              // Estilo do botão
+              child: Text('LOGIN'), // Texto exibido dentro do botão
+              style: ElevatedButton.styleFrom( 
+                backgroundColor: Colors.white, // Cor de fundo do botão 
+                foregroundColor: Colors.blue, // Cor do texto 
               ),
             )
           ],
