477d04acd8f8c73d3c62c1cffc5dbfdd7f9b9dac
isabellacr444 <0001114399279sp@al.educacao.sp.gov.br>
2025-10-08 11:27:52 -0300

    08.10

---
+++ Hardware/sa_geolocator_maps/lib/models/point_location.dart
@@ -0,0 +1,15 @@
+//classe para organizar os pontos de marcadores
+
+class PointLocation {
+  //atributos
+  final double latitude;
+  final double longitude;
+  final String dataHora;
+
+  //Construtor
+  PointLocation({
+    required this.latitude,
+    required this.longitude,
+    required this.dataHora,
+  });
+}
